844007094954a3e2093ce1381211c341f62df1e2
Aliaksandr Drazhin <Draghin2@gmail.com>
2021-03-15 21:47:33 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
-}
+    // let rev = n.toString().split('');
+    // let arr = Array.from(rev).reverse();
+    // let res;
+    // if (arr.includes('-')) {
+    //     arr.pop();
+    //     res = arr.join('');
+    // } else {
+    //     res = arr.join('');
+    // }
+    // return res;
+    return [...`${n < 0 ? n*(-1) : n}`].reverse().join('');
+};
